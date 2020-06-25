abf81e2a375daf3eee449c4c96a7269a43ecd8ca
Ryutooooo <rf.ryutooooo.0907@gmail.com>
2020-06-26 02:15:42 +0900

    fix: condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,9 @@ if [ -z "$ASSIGNEES" ]; then
   exit 1
 fi
 
+add_section "this time assignees"
+echo $ASSIGNEES
+
 find_project_id() {
   _PROJECT_TYPE="$1"
   _PROJECT_URL="$2"
@@ -88,6 +91,9 @@ TARGET_PROJECTS=$(curl -s -X GET -u "$ACTOR:$TOKEN" --retry 3 \
   "https://api.github.com/orgs/${ORG_NAME}/projects" | \
   jq '.[] | select( .body | contains("KANBAN") ) | { name: .name, desc: .body, url: .html_url }' | jq -s)
 
+add_section "got target projects"
+echo "projects: $TARGET_PROJECTS"
+
 len=$(echo $TARGET_PROJECTS | jq length)
 
 for i in $( seq 0 $(($len - 1)) )
@@ -95,47 +101,52 @@ do
   CURRENT_PROJECT=$(echo $TARGET_PROJECTS | jq .[${i}])
   TARGET_TEAM=$(echo $CURRENT_PROJECT | jq .desc | cut -d '*' -f 3 | cut -d ':' -f 2)
   add_section "got target team"
+  echo "target team: $TARGET_TEAM"
 
   MEMBERS=$(curl -s -X GET -u "$ACTOR:$TOKEN" --retry 3 \
     -H "Content-Type: application/json" \
     "https://api.github.com/orgs/${ORG_NAME}/teams/${TARGET_TEAM}/members" | jq .[].login)
+
   add_section "got target team members"
+  echo "members: $MEMBERS"
 
 
+  for assignee in $ASSIGNEES
+  do
+    if echo $MEMBERS | grep $assignee; then 
+      add_section "$assignee was included in the target team"
 
-  if [ "`echo $MEMBERS | grep $ASSIGNEES`" ]; then
-    add_section "The person assigned this time was included in the target team"
+      PROJECT_URL=$(echo $CURRENT_PROJECT | jq .url)
+      # TODO: handle any project type
+      PROJECT_TYPE=org
+      PROJECT_ID=$(find_project_id "$PROJECT_TYPE" "$PROJECT_URL")
+      INITIAL_COLUMN_ID=$(find_column_id $PROJECT_ID "${INITIAL_COLUMN_NAME:?<Error> required this environment variable}")
 
-    PROJECT_URL=$(echo $CURRENT_PROJECT | jq .url)
-    # TODO: handle any project type
-    PROJECT_TYPE=org
-    PROJECT_ID=$(find_project_id "$PROJECT_TYPE" "$PROJECT_URL")
-    INITIAL_COLUMN_ID=$(find_column_id $PROJECT_ID "${INITIAL_COLUMN_NAME:?<Error> required this environment variable}")
+      case "$GITHUB_EVENT_NAME" in
+        issues)
+          ISSUE_ID=$(jq -r '.issue.id' < "$GITHUB_EVENT_PATH")
 
-		case "$GITHUB_EVENT_NAME" in
-			issues)
-				ISSUE_ID=$(jq -r '.issue.id' < "$GITHUB_EVENT_PATH")
+          # Add this issue to the project column
+          curl -s -X POST -u "$ACTOR:$TOKEN" --retry 3 \
+           -H 'Accept: application/vnd.github.inertia-preview+json' \
+           -d "{\"content_type\": \"Issue\", \"content_id\": $ISSUE_ID}" \
+           "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
+          ;;
+        pull_request)
+          PULL_REQUEST_ID=$(jq -r '.pull_request.id' < "$GITHUB_EVENT_PATH")
 
-				# Add this issue to the project column
-				curl -s -X POST -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
-				 -H 'Accept: application/vnd.github.inertia-preview+json' \
-				 -d "{\"content_type\": \"Issue\", \"content_id\": $ISSUE_ID}" \
-				 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
-				;;
-			pull_request)
-				PULL_REQUEST_ID=$(jq -r '.pull_request.id' < "$GITHUB_EVENT_PATH")
-
-				# Add this pull_request to the project column
-				curl -s -X POST -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
-				 -H 'Accept: application/vnd.github.inertia-preview+json' \
-				 -d "{\"content_type\": \"PullRequest\", \"content_id\": $PULL_REQUEST_ID}" \
-				 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
-				;;
-			*)
-				echo "Nothing to be done on this action: $GITHUB_EVENT_NAME" >&2
-				exit 1
-				;;
-		esac
-  fi
+          # Add this pull_request to the project column
+          curl -s -X POST -u "$ACTOR:$TOKEN" --retry 3 \
+           -H 'Accept: application/vnd.github.inertia-preview+json' \
+           -d "{\"content_type\": \"PullRequest\", \"content_id\": $PULL_REQUEST_ID}" \
+           "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
+          ;;
+        *)
+          echo "Nothing to be done on this action: $GITHUB_EVENT_NAME" >&2
+          exit 1
+          ;;
+      esac
+    fi
+  done
   unset section_count
 done
